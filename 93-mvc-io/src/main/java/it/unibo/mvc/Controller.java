61fb03fbb983c9e05e8da3127f9470907b4fa77f
Simone Salucci <simone.salucci3@studio,unibo.it>
2025-12-06 13:57:50 +0100

    Exercise 93: Controller interface implementation

--- 93-mvc-io/src/main/java/it/unibo/mvc/Controller.java
+++ 93-mvc-io/src/main/java/it/unibo/mvc/Controller.java
@@ -1,8 +1,31 @@
 package it.unibo.mvc;
 
+import java.util.List;
+
 /**
  *
  */
 public interface Controller {
 
+    /**
+    * @param next the next string to print, must not be null
+    * @throws NullPointerException if next is null
+    */
+    public void setNextString(String next);
+
+    /**
+    * @return the next string that will be printed
+    */
+    public String getNextString();
+
+    /**
+    * @return a List of string that rapresents the history of printed string
+    */
+    public List<String> getHistory();
+
+    /**
+    * Print the current string to the standard output
+    * @throws IllegalStateException if the current string is unset
+    */
+    public String printCurrentString();
 }
